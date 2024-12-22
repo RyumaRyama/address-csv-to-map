8853da7e90b763148dee6603d113d3fce6d318b2
RyumaRyama <ryumaryama@gmail.com>
2024-12-22 22:09:07 +0900

    画面に必要要素追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
+import PREF_OPTIONS from "@/components/prefectures";
 import { MapComponent } from "../components/MapComponent/index";
 
 export default function Home() {
   return (
-    <div className="flex justify-center">
-      <MapComponent />
-    </div>
+    <>
+      <div>
+        <div className="flex justify-center">
+          <h1 className="text-2xl mt-5 mb-3">CSV to MAP</h1>
+        </div>
+        <div className="flex justify-center">
+          <select className="w-30 p-2 m-2 border">
+            {PREF_OPTIONS().map((pref, index) => (
+              <option key={index} value={pref}>
+                {pref}
+              </option>
+            ))}
+          </select>
+          <input className="w-30 p-2 m-2 text border"></input>
+        </div>
+        <div className="flex justify-center">
+          <input type="file" className="w-30 p-2 m-2"></input>
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <MapComponent />
+      </div>
+    </>
   );
 }
